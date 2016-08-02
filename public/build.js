8ff82da5f1d7cab3ecb6e04d282017b8f1029843
jhar <jhar87@gmail.com>
2016-08-01 23:23:21 -0500

    Work on Resume portion of site

--- public/build.js
+++ public/build.js
@@ -73,11 +73,11 @@
 
 	var _Resume2 = _interopRequireDefault(_Resume);
 
-	var _Blog = __webpack_require__(11);
+	var _Blog = __webpack_require__(15);
 
 	var _Blog2 = _interopRequireDefault(_Blog);
 
-	var _vueRouter = __webpack_require__(17);
+	var _vueRouter = __webpack_require__(21);
 
 	var _vueRouter2 = _interopRequireDefault(_vueRouter);
 
@@ -10697,7 +10697,8 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var __vue_script__, __vue_template__
-	__vue_template__ = __webpack_require__(10)
+	__webpack_require__(10)
+	__vue_template__ = __webpack_require__(14)
 	module.exports = __vue_script__ || {}
 	if (module.exports.__esModule) module.exports = module.exports.default
 	if (__vue_template__) { (typeof module.exports === "function" ? module.exports.options : module.exports).template = __vue_template__ }
@@ -10715,17 +10716,365 @@
 
 /***/ },
 /* 10 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	module.exports = "\n  <h2>Resume placeholder</h2>\n";
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(11);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(13)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./../../node_modules/vue-loader/lib/style-rewriter.js?id=_v-13403013&file=Resume.vue!./../../node_modules/sass-loader/index.js?indentedSyntax!./../../node_modules/vue-loader/lib/selector.js?type=style&index=0!./Resume.vue", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./../../node_modules/vue-loader/lib/style-rewriter.js?id=_v-13403013&file=Resume.vue!./../../node_modules/sass-loader/index.js?indentedSyntax!./../../node_modules/vue-loader/lib/selector.js?type=style&index=0!./Resume.vue");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
 
 /***/ },
 /* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
+	exports = module.exports = __webpack_require__(12)();
+	// imports
+
+
+	// module
+	exports.push([module.id, ".my-resume {\n  width: 100%; }\n  .my-resume p {\n    color: #676767;\n    font-size: 14px; }\n  .my-resume .bold {\n    font-weight: bold; }\n  .my-resume .col-1 {\n    display: inline-block;\n    float: left;\n    width: 62%; }\n    .my-resume .col-1 .italic {\n      font-style: italic; }\n    .my-resume .col-1 .dates {\n      color: #676767;\n      font-size: 10px; }\n  .my-resume .col-2 {\n    display: inline-block;\n    float: right;\n    width: 38%; }\n", ""]);
+
+	// exports
+
+
+/***/ },
+/* 12 */
+/***/ function(module, exports) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	// css base code, injected by the css-loader
+	module.exports = function() {
+		var list = [];
+
+		// return the list of modules as css string
+		list.toString = function toString() {
+			var result = [];
+			for(var i = 0; i < this.length; i++) {
+				var item = this[i];
+				if(item[2]) {
+					result.push("@media " + item[2] + "{" + item[1] + "}");
+				} else {
+					result.push(item[1]);
+				}
+			}
+			return result.join("");
+		};
+
+		// import a list of modules into the list
+		list.i = function(modules, mediaQuery) {
+			if(typeof modules === "string")
+				modules = [[null, modules, ""]];
+			var alreadyImportedModules = {};
+			for(var i = 0; i < this.length; i++) {
+				var id = this[i][0];
+				if(typeof id === "number")
+					alreadyImportedModules[id] = true;
+			}
+			for(i = 0; i < modules.length; i++) {
+				var item = modules[i];
+				// skip already imported module
+				// this implementation is not 100% perfect for weird media query combinations
+				//  when a module is imported multiple times with different media queries.
+				//  I hope this will never occur (Hey this way we have smaller bundles)
+				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+					if(mediaQuery && !item[2]) {
+						item[2] = mediaQuery;
+					} else if(mediaQuery) {
+						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+					}
+					list.push(item);
+				}
+			}
+		};
+		return list;
+	};
+
+
+/***/ },
+/* 13 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	var stylesInDom = {},
+		memoize = function(fn) {
+			var memo;
+			return function () {
+				if (typeof memo === "undefined") memo = fn.apply(this, arguments);
+				return memo;
+			};
+		},
+		isOldIE = memoize(function() {
+			return /msie [6-9]\b/.test(window.navigator.userAgent.toLowerCase());
+		}),
+		getHeadElement = memoize(function () {
+			return document.head || document.getElementsByTagName("head")[0];
+		}),
+		singletonElement = null,
+		singletonCounter = 0,
+		styleElementsInsertedAtTop = [];
+
+	module.exports = function(list, options) {
+		if(false) {
+			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
+		}
+
+		options = options || {};
+		// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+		// tags it will allow on a page
+		if (typeof options.singleton === "undefined") options.singleton = isOldIE();
+
+		// By default, add <style> tags to the bottom of <head>.
+		if (typeof options.insertAt === "undefined") options.insertAt = "bottom";
+
+		var styles = listToStyles(list);
+		addStylesToDom(styles, options);
+
+		return function update(newList) {
+			var mayRemove = [];
+			for(var i = 0; i < styles.length; i++) {
+				var item = styles[i];
+				var domStyle = stylesInDom[item.id];
+				domStyle.refs--;
+				mayRemove.push(domStyle);
+			}
+			if(newList) {
+				var newStyles = listToStyles(newList);
+				addStylesToDom(newStyles, options);
+			}
+			for(var i = 0; i < mayRemove.length; i++) {
+				var domStyle = mayRemove[i];
+				if(domStyle.refs === 0) {
+					for(var j = 0; j < domStyle.parts.length; j++)
+						domStyle.parts[j]();
+					delete stylesInDom[domStyle.id];
+				}
+			}
+		};
+	}
+
+	function addStylesToDom(styles, options) {
+		for(var i = 0; i < styles.length; i++) {
+			var item = styles[i];
+			var domStyle = stylesInDom[item.id];
+			if(domStyle) {
+				domStyle.refs++;
+				for(var j = 0; j < domStyle.parts.length; j++) {
+					domStyle.parts[j](item.parts[j]);
+				}
+				for(; j < item.parts.length; j++) {
+					domStyle.parts.push(addStyle(item.parts[j], options));
+				}
+			} else {
+				var parts = [];
+				for(var j = 0; j < item.parts.length; j++) {
+					parts.push(addStyle(item.parts[j], options));
+				}
+				stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
+			}
+		}
+	}
+
+	function listToStyles(list) {
+		var styles = [];
+		var newStyles = {};
+		for(var i = 0; i < list.length; i++) {
+			var item = list[i];
+			var id = item[0];
+			var css = item[1];
+			var media = item[2];
+			var sourceMap = item[3];
+			var part = {css: css, media: media, sourceMap: sourceMap};
+			if(!newStyles[id])
+				styles.push(newStyles[id] = {id: id, parts: [part]});
+			else
+				newStyles[id].parts.push(part);
+		}
+		return styles;
+	}
+
+	function insertStyleElement(options, styleElement) {
+		var head = getHeadElement();
+		var lastStyleElementInsertedAtTop = styleElementsInsertedAtTop[styleElementsInsertedAtTop.length - 1];
+		if (options.insertAt === "top") {
+			if(!lastStyleElementInsertedAtTop) {
+				head.insertBefore(styleElement, head.firstChild);
+			} else if(lastStyleElementInsertedAtTop.nextSibling) {
+				head.insertBefore(styleElement, lastStyleElementInsertedAtTop.nextSibling);
+			} else {
+				head.appendChild(styleElement);
+			}
+			styleElementsInsertedAtTop.push(styleElement);
+		} else if (options.insertAt === "bottom") {
+			head.appendChild(styleElement);
+		} else {
+			throw new Error("Invalid value for parameter 'insertAt'. Must be 'top' or 'bottom'.");
+		}
+	}
+
+	function removeStyleElement(styleElement) {
+		styleElement.parentNode.removeChild(styleElement);
+		var idx = styleElementsInsertedAtTop.indexOf(styleElement);
+		if(idx >= 0) {
+			styleElementsInsertedAtTop.splice(idx, 1);
+		}
+	}
+
+	function createStyleElement(options) {
+		var styleElement = document.createElement("style");
+		styleElement.type = "text/css";
+		insertStyleElement(options, styleElement);
+		return styleElement;
+	}
+
+	function createLinkElement(options) {
+		var linkElement = document.createElement("link");
+		linkElement.rel = "stylesheet";
+		insertStyleElement(options, linkElement);
+		return linkElement;
+	}
+
+	function addStyle(obj, options) {
+		var styleElement, update, remove;
+
+		if (options.singleton) {
+			var styleIndex = singletonCounter++;
+			styleElement = singletonElement || (singletonElement = createStyleElement(options));
+			update = applyToSingletonTag.bind(null, styleElement, styleIndex, false);
+			remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true);
+		} else if(obj.sourceMap &&
+			typeof URL === "function" &&
+			typeof URL.createObjectURL === "function" &&
+			typeof URL.revokeObjectURL === "function" &&
+			typeof Blob === "function" &&
+			typeof btoa === "function") {
+			styleElement = createLinkElement(options);
+			update = updateLink.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+				if(styleElement.href)
+					URL.revokeObjectURL(styleElement.href);
+			};
+		} else {
+			styleElement = createStyleElement(options);
+			update = applyToTag.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+			};
+		}
+
+		update(obj);
+
+		return function updateStyle(newObj) {
+			if(newObj) {
+				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
+					return;
+				update(obj = newObj);
+			} else {
+				remove();
+			}
+		};
+	}
+
+	var replaceText = (function () {
+		var textStore = [];
+
+		return function (index, replacement) {
+			textStore[index] = replacement;
+			return textStore.filter(Boolean).join('\n');
+		};
+	})();
+
+	function applyToSingletonTag(styleElement, index, remove, obj) {
+		var css = remove ? "" : obj.css;
+
+		if (styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = replaceText(index, css);
+		} else {
+			var cssNode = document.createTextNode(css);
+			var childNodes = styleElement.childNodes;
+			if (childNodes[index]) styleElement.removeChild(childNodes[index]);
+			if (childNodes.length) {
+				styleElement.insertBefore(cssNode, childNodes[index]);
+			} else {
+				styleElement.appendChild(cssNode);
+			}
+		}
+	}
+
+	function applyToTag(styleElement, obj) {
+		var css = obj.css;
+		var media = obj.media;
+
+		if(media) {
+			styleElement.setAttribute("media", media)
+		}
+
+		if(styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = css;
+		} else {
+			while(styleElement.firstChild) {
+				styleElement.removeChild(styleElement.firstChild);
+			}
+			styleElement.appendChild(document.createTextNode(css));
+		}
+	}
+
+	function updateLink(linkElement, obj) {
+		var css = obj.css;
+		var sourceMap = obj.sourceMap;
+
+		if(sourceMap) {
+			// http://stackoverflow.com/a/26603875
+			css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
+		}
+
+		var blob = new Blob([css], { type: "text/css" });
+
+		var oldSrc = linkElement.href;
+
+		linkElement.href = URL.createObjectURL(blob);
+
+		if(oldSrc)
+			URL.revokeObjectURL(oldSrc);
+	}
+
+
+/***/ },
+/* 14 */
+/***/ function(module, exports) {
+
+	module.exports = "\n  <div class=\"my-resume\">\n    <div class=\"col-1\">\n      <h1>Justin Harrison</h1>\n      <h2>Web Application Developer</h2>\n      <h3>Web Experience</h3>\n      <span class=\"bold\">Golden Funnel Marketing</span>, Remote - <span class=\"italic\">Web Engineer</span><br>\n      <span class=\"dates\">FEBRUARY 2016 - JULY 2017</span><br>\n      <p>Full stack developer for a wide variety of digital marketing projects. Rapid development of sites and services to the architecture and specifications of a non-technical executive. Engineering custom solutions to integration problems between WordPress, WP plugins, and external services. Third party JavaScript development. API design, development, and testing.</p>\n      <span class=\"bold\">Not That, This LLC</span>, Remote — <span class=\"italic\">Software Developer</span><br>\n      <span class=\"dates\">NOVEMBER 2015 - JANUARY 2016</span><br>\n      <p>Contract JavaScript development for Cisco project based in Dubai. Converted PSD designs to pixel perfect UI for an existing enterprise level IoT interface written with Enyo. Reported directly to C-level executives.</p>\n      <span class=\"bold\">FlexaSoft</span>, Remote — <span class=\"italic\">Web Content Evaluator</span><br>\n      <span class=\"dates\">OCTOBER 2014 - JUNE 2015</span><br>\n      <p>Human judge for tuning Microsoft Bing’s semantic search results.</p>\n      <span class=\"bold\">UC Santa Barbara</span>, Santa Barbara, CA — <span class=\"italic\">Web Developer</span><br>\n      <span class=\"dates\">JULY 2014 - SEPTEMBER 2014</span><br>\n      <p>WordPress theme development and site mergers for a UC wide arts research institute.</p>\n      <h3>Education</h3>\n      <span class=\"bold\">Udacity</span>, Remote — <span class=\"italic\">Front End Development Nanodegree</span>\n      <p>HTML5, Git/Github, AJAX, CSS/Bootstrap, jQuery, Object-oriented design/development, Design Patterns, Test Driven Development, Performance Optimization, Application Development/Deployment</p>\n      <span class=\"bold\">UC Santa Barbara</span>, Santa Barbara, CA — <span class=\"italic\">BA Art, High Honors</span>\n      <p>Processing, Arduino, Drawing, Painting, Printmaking</p>\n      University of Georgia, Athens, GA — <span class=\"italic\">Cognitive Science</span>\n      <p>Logic, Java, C++, Unix</p>\n    </div>\n    <div class=\"col-2\">\n      <span class=\"bold\">(206) 377-9027</span><br>\n      <span class=\"bold\">Skype: justin.a.harrison</span><br>\n      <a href=\"mailto:jhar87@gmail.com\" target=\"_top\">jhar87@gmail.com</a><br>\n      <h3>Primary Skills</h3>\n      <p>JavaScript, CSS, HTML, PHP, MySQL and MongoDB.</p>\n      <p>Experience using a wide variety of libraries, frameworks, platforms, and tools related to the above core technologies (Node/NPM, React, Bootstrap, Laravel, AWS, etc). Experience with other languages such as Python, C, C++, and Java but not in a professional context.</p>\n      <p>Experience balancing client requests with architectural concerns, design patterns, and best practices - learning from apps that don’t scale or fail to be maintainable.</p>\n      <h3>Awards</h3>\n      <span class=\"bold\">Promising Artist Achievement Award</span><br>\n      UCSB Department of Art\n      <p>Most outstanding graduating artist</p>\n      <span class=\"bold\">William Dole Award</span><br>\n      UCSB Department of Art<br><br>\n      <span class=\"bold\">Faculty Award of Distinction</span><br>\n      UCSB Department of Art<br><br>\n      <span class=\"bold\">Dean's Honors x5</span><br>\n      UCSB\n    </div>\n  </div>\n";
+
+/***/ },
+/* 15 */
+/***/ function(module, exports, __webpack_require__) {
+
 	var __vue_script__, __vue_template__
-	__vue_script__ = __webpack_require__(12)
-	__vue_template__ = __webpack_require__(16)
+	__vue_script__ = __webpack_require__(16)
+	__vue_template__ = __webpack_require__(20)
 	module.exports = __vue_script__ || {}
 	if (module.exports.__esModule) module.exports = module.exports.default
 	if (__vue_template__) { (typeof module.exports === "function" ? module.exports.options : module.exports).template = __vue_template__ }
@@ -10742,7 +11091,7 @@
 	})()}
 
 /***/ },
-/* 12 */
+/* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -10751,11 +11100,11 @@
 	  value: true
 	});
 
-	var _firebase = __webpack_require__(13);
+	var _firebase = __webpack_require__(17);
 
 	var firebase = _interopRequireWildcard(_firebase);
 
-	var _marked = __webpack_require__(15);
+	var _marked = __webpack_require__(19);
 
 	var _marked2 = _interopRequireDefault(_marked);
 
@@ -10906,7 +11255,7 @@
 	//
 
 /***/ },
-/* 13 */
+/* 17 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/**
@@ -10916,12 +11265,12 @@
 	 *
 	 *   firebase = require('firebase');
 	 */
-	__webpack_require__(14);
+	__webpack_require__(18);
 	module.exports = firebase;
 
 
 /***/ },
-/* 14 */
+/* 18 */
 /***/ function(module, exports) {
 
 	/* WEBPACK VAR INJECTION */(function(global) {/*! @license Firebase v3.2.1
@@ -11495,7 +11844,7 @@
 	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ },
-/* 15 */
+/* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(global) {/**
@@ -12788,13 +13137,13 @@
 	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ },
-/* 16 */
+/* 20 */
 /***/ function(module, exports) {
 
 	module.exports = "\n  <div class=\"blog\">\n    <ul>\n      <li v-for=\"post in posts | orderBy 'dateCreated' -1\" track-by=\".key\">\n        <h3>{{ post.title }}</h3>\n        <span>{{ post.category }}</span><br>\n        <span>Created: {{ post.dateCreated | dated }}</span><br>\n        <span v-if=\"post.dateUpdated\">Updated: {{ post.dateUpdated | dated }}</span><br>\n        <button v-if=\"user.email === 'jhar87@gmail.com'\" @click=\"toggleEditPost(post['.key'])\">Edit Post</button>\n        <button v-if=\"user.email === 'jhar87@gmail.com'\" @click=\"deletePost(post['.key'])\">Delete Post</button>\n        <div>{{{ post.content | marked }}}</div>\n      </li>\n    </ul>\n    <div v-if=\"user\">\n      <ul>\n        <li v-if=\"user.email\">{{user.email}}</li>\n        <li v-if=\"user.email === 'jhar87@gmail.com'\">\n          <button @click=\"toggleCreatePost\">Create Post</button>\n        </li>\n        <li>\n          <button v-on:click=\"signOut\">Sign Out</button>\n        </li>\n      </ul>\n      <form v-if=\"user.email === 'jhar87@gmail.com' && showCreatePost === true\" @submit.prevent=\"setPost\">\n        <input v-model=\"newPost.title\" placeholder=\"Post title\"><br>\n        <input v-model=\"newPost.category\" placeholder=\"Post category\"><br>\n        <textarea v-model=\"newPost.content\" placeholder=\"Post content\"></textarea><br>\n        <input v-model=\"newPost.key\" placeholder=\"Pretty url\"><br>\n        <button>Add Post</button>\n      </form>\n      <form v-if=\"user.email === 'jhar87@gmail.com' && showEditPost === true\" @submit.prevent=\"setPost\">\n        <input v-model=\"newPost.title\"><br>\n        <input v-model=\"newPost.category\"><br>\n        <textarea v-model=\"newPost.content\"></textarea><br>\n        <input v-model=\"newPost.key\"><br>\n        <button>Submit Edit</button>\n      </form>\n    </div>\n    <div v-else>\n      <button @click=\"signIn\">Sign In</button>\n    </div>\n  </div>\n";
 
 /***/ },
-/* 17 */
+/* 21 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*!
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,5 +1,76 @@
-<!-- src/components/Resume.vue -->
+<style lang="sass?indentedSyntax">
+.my-resume
+  width: 100%
+
+  p
+    color: #676767
+    font-size: 14px
+
+  .bold
+    font-weight: bold
+    
+  .col-1
+    display: inline-block
+    float: left
+    width: 62%
+
+    .italic
+      font-style: italic
+    
+    .dates
+      color: #676767
+      font-size: 10px
+
+  .col-2
+    display: inline-block
+    float: right
+    width: 38%
+</style>
 
 <template>
-  <h2>Resume placeholder</h2>
+  <div class="my-resume">
+    <div class="col-1">
+      <h1>Justin Harrison</h1>
+      <h2>Web Application Developer</h2>
+      <h3>Web Experience</h3>
+      <span class="bold">Golden Funnel Marketing</span>, Remote - <span class="italic">Web Engineer</span><br>
+      <span class="dates">FEBRUARY 2016 - JULY 2017</span><br>
+      <p>Full stack developer for a wide variety of digital marketing projects. Rapid development of sites and services to the architecture and specifications of a non-technical executive. Engineering custom solutions to integration problems between WordPress, WP plugins, and external services. Third party JavaScript development. API design, development, and testing.</p>
+      <span class="bold">Not That, This LLC</span>, Remote — <span class="italic">Software Developer</span><br>
+      <span class="dates">NOVEMBER 2015 - JANUARY 2016</span><br>
+      <p>Contract JavaScript development for Cisco project based in Dubai. Converted PSD designs to pixel perfect UI for an existing enterprise level IoT interface written with Enyo. Reported directly to C-level executives.</p>
+      <span class="bold">FlexaSoft</span>, Remote — <span class="italic">Web Content Evaluator</span><br>
+      <span class="dates">OCTOBER 2014 - JUNE 2015</span><br>
+      <p>Human judge for tuning Microsoft Bing’s semantic search results.</p>
+      <span class="bold">UC Santa Barbara</span>, Santa Barbara, CA — <span class="italic">Web Developer</span><br>
+      <span class="dates">JULY 2014 - SEPTEMBER 2014</span><br>
+      <p>WordPress theme development and site mergers for a UC wide arts research institute.</p>
+      <h3>Education</h3>
+      <span class="bold">Udacity</span>, Remote — <span class="italic">Front End Development Nanodegree</span>
+      <p>HTML5, Git/Github, AJAX, CSS/Bootstrap, jQuery, Object-oriented design/development, Design Patterns, Test Driven Development, Performance Optimization, Application Development/Deployment</p>
+      <span class="bold">UC Santa Barbara</span>, Santa Barbara, CA — <span class="italic">BA Art, High Honors</span>
+      <p>Processing, Arduino, Drawing, Painting, Printmaking</p>
+      <span class="bold">University of Georgia</span>, Athens, GA — <span class="italic">Cognitive Science</span>
+      <p>Logic, Java, C++, Unix</p>
+    </div>
+    <div class="col-2">
+      <span class="bold">(206) 377-9027</span><br>
+      <span class="bold">Skype: justin.a.harrison</span><br>
+      <a href="mailto:jhar87@gmail.com" target="_top">jhar87@gmail.com</a><br>
+      <h3>Primary Skills</h3>
+      <p>JavaScript, CSS, HTML, PHP, MySQL and MongoDB.</p>
+      <p>Experience using a wide variety of libraries, frameworks, platforms, and tools related to the above core technologies (Node/NPM, React, Bootstrap, Laravel, AWS, etc). Experience with other languages such as Python, C, C++, and Java but not in a professional context.</p>
+      <p>Experience balancing client requests with architectural concerns, design patterns, and best practices - learning from apps that don’t scale or fail to be maintainable.</p>
+      <h3>Awards</h3>
+      <span class="bold">Promising Artist Achievement Award</span><br>
+      UCSB Department of Art
+      <p>Most outstanding graduating artist</p>
+      <span class="bold">William Dole Award</span><br>
+      UCSB Department of Art<br><br>
+      <span class="bold">Faculty Award of Distinction</span><br>
+      UCSB Department of Art<br><br>
+      <span class="bold">Dean's Honors x5</span><br>
+      UCSB
+    </div>
+  </div>
 </template>
